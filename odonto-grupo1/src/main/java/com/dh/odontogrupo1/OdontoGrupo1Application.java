2760d20b26e22fe394d39feaf56f69800e3c458c
ezioweb <ezioweb@hotmail.com>
2022-08-29 20:37:59 -0300

    Primeiro commit

---
+++ odonto-grupo1/src/main/java/com/dh/odontogrupo1/OdontoGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.dh.odontogrupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontoGrupo1Application.class, args);
+	}
+
+}
---
+++ odonto-grupo1/src/test/java/com/dh/odontogrupo1/OdontoGrupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.odontogrupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoGrupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
