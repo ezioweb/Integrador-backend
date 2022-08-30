4e558617e19a0021e2a3d996a2c53386916e5852
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-08-29 22:40:53 -0300

    add script de criacao da tabela consulta

--- odonto-grupo1/create.sql
+++ odonto-grupo1/create.sql
@@ -1,22 +1,32 @@
 create table if not exists endereco(
        id int auto_increment primary key,
-       rua varchar(100),
-       numero varchar(100),
-       bairro varchar(100)
+       rua varchar(100) not null,
+       numero varchar(100) not null,
+       bairro varchar(100) not null
 );
 
 create table if not exists paciente(
     id int auto_increment primary key,
-    nome varchar(100),
-    sobrenome varchar(100),
-    endereco varchar(100),
-    rg varchar(100),
+    nome varchar(100) not null,
+    sobrenome varchar(100) not null,
+    endereco varchar(100) not null,
+    rg varchar(100) not null,
     datacadastro current_timestamp()
 );
 
 create table if not exists dentista(
-    int id auto_increment primary key,
-    nome varchar(100),
-    sobrenome varchar(100),
-    matricula varchar(100)
+    id int auto_increment primary key,
+    nome varchar(100) not null,
+    sobrenome varchar(100) not null,
+    matricula varchar(100) not null
+);
+
+create table if not exists consulta(
+    id int auto_increment primary key,
+    id_dentista int,
+    id_paciente int,
+    foreign key (id_dentista) references dentista(id),
+    foreign key (id_paciente) references paciente(id),
+    datahoraconsulta timestamp
+
 );
