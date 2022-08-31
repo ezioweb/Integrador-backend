0680a780b3ffc52e794b2bfab5007f3f83e51f7f
ezioweb <ezioweb@hotmail.com>
2022-08-31 19:44:17 -0300

    commit PacienteDAOH2

--- odonto-grupo1/src/main/java/com/dh/odontogrupo1/dao/impl/PacienteDAOH2.java
+++ odonto-grupo1/src/main/java/com/dh/odontogrupo1/dao/impl/PacienteDAOH2.java
@@ -2,17 +2,13 @@ package com.dh.odontogrupo1.dao.impl;
 
 import com.dh.odontogrupo1.dao.ConfiguracaoJDBC;
 import com.dh.odontogrupo1.dao.IDao;
-import com.dh.odontogrupo1.model.Dentista;
 import com.dh.odontogrupo1.model.Paciente;
 import org.apache.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -80,17 +76,79 @@ public class PacienteDAOH2 implements IDao<Paciente> {
 
     @Override
     public void alterar(Paciente paciente) throws SQLException {
+        log.info("Abrindo conexão");
+        String sqlUpdate = String.format("UPDATE paciente SET rg = '%s' where id = '%s';",
+                paciente.getRg(), paciente.getId());
+        Connection connection = null;
+
+        try {
+            log.info("Alterando o RG do id: " + paciente.getId());
+            configuracaoJDBC = new ConfiguracaoJDBC("org.h2.Driver","jdbc:h2:~/odontoclinica;INIT=RUNSCRIPT FROM 'create.sql'","sa","");
+            connection = configuracaoJDBC.getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(sqlUpdate);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Erro ao alterar RG: " + e.getMessage());
+        } finally {
+            log.info("Fechando conexão");
+            connection.close();
+        }
 
     }
 
     @Override
     public Optional<Paciente> buscarPorId(int id) throws SQLException {
-        return Optional.empty();
+        log.debug("Abrindo uma conexão");
+        Connection connection = null;
+        Statement stmt = null;
+        String query = String.format("SELECT * FROM paciente WHERE ID = %s",id);
+        Paciente paciente = null;
+
+        try {
+            configuracaoJDBC = new ConfiguracaoJDBC("org.h2.Driver","jdbc:h2:~/odontoclinica;INIT=RUNSCRIPT FROM 'create.sql'","sa","");
+            connection = configuracaoJDBC.getConnection();
+
+            log.debug("Buscando paciente de id: " + id);
+            stmt = connection.createStatement();
+            ResultSet resultado = stmt.executeQuery(query);
+
+            while (resultado.next()) {
+                paciente = criarPaciente(resultado);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error("Erro ao buscar dentista: " + e.getMessage());
+        }finally {
+
+            log.debug("Fechando a conexão");
+            connection.close();
+        }
+
+        return paciente != null ? Optional.of(paciente) : Optional.empty();
     }
 
     @Override
     public void excluir(int id) throws SQLException {
-
+        log.info("Abrindo Conexão");
+        Connection conn = null;
+        Statement stmt = null;
+        String sqlDelete = String.format("DELETE FROM paciente WHERE id='%s'", id);
+        try{
+            configuracaoJDBC = new ConfiguracaoJDBC("org.h2.Driver","jdbc:h2:~/odontoclinica;INIT=RUNSCRIPT FROM 'create.sql'","sa","");
+            conn = configuracaoJDBC.getConnection();
+            stmt = conn.createStatement();
+            stmt.execute(sqlDelete);
+            log.debug("Excluindo paciente de id: " + id);
+        } catch(SQLException e){
+            e.printStackTrace();
+            log.error("Erro ao excluir paciente: " + e.getMessage());
+        } finally {
+            log.info("Fechando conexão");
+            conn.close();
+        }
     }
 
     private Paciente criarPaciente(ResultSet resultSet) throws SQLException {
