b6a45dd6f844f78805a91d118835d8c707312f04
danielspiker <danielmnascimento@gmail.com>
2022-09-12 19:46:46 -0300

    add junit

---
+++ odonto-grupo1/src/test/java/com/dh/odontogrupo1/DentistaServiceTest.java
@@ -0,0 +1,12 @@
+package com.dh.odontogrupo1;
+
+import com.dh.odontogrupo1.service.DentistaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DentistaServiceTest {
+
+    @Autowired
+    DentistaService dentistaService;
+}
