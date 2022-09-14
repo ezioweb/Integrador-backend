a055fe6ec692b5a38bb50e10a5b70a4a73247276
luksm201 <lucas_mbsilva@hotmail.com>
2022-09-13 22:36:49 -0300

    PacienteServiceTest

--- odonto-grupo1/src/test/java/com/dh/odontogrupo1/PacienteServiceTest.java
+++ odonto-grupo1/src/test/java/com/dh/odontogrupo1/PacienteServiceTest.java
@@ -2,9 +2,9 @@ package com.dh.odontogrupo1;
 
 import com.dh.odontogrupo1.model.Endereco;
 import com.dh.odontogrupo1.model.Paciente;
-import com.dh.odontogrupo1.service.DentistaService;
 import com.dh.odontogrupo1.service.EnderecoService;
 import com.dh.odontogrupo1.service.PacienteService;
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,14 +15,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PacienteServiceTest {
 
     @Autowired
-    PacienteService pacienteService = new PacienteService();
+    PacienteService pacienteService;
 
+    final static Logger log = Logger.getLogger(PacienteServiceTest.class);
     static Paciente paciente = new Paciente();
 
     static Endereco endereco = new Endereco();
 
-
-    static EnderecoService enderecoService;
 
     static Endereco enderecoSalvo;
 
@@ -33,22 +32,21 @@ public class PacienteServiceTest {
         endereco.setRua("Avenida do Tetra");
         endereco.setBairro("Jd Brasil");
         endereco.setNumero("123");
-
-        enderecoSalvo = enderecoService.salvarEndereco(endereco);
+        log.info("Criando endereço: " + endereco);
 
 
         paciente.setNome("Ayrton");
         paciente.setSobrenome("Senna");
         paciente.setRg("123456");
-        paciente.setEndereco(enderecoSalvo);
+        paciente.setEndereco(endereco);
     }
 
     @Test
     void salvamentoPacienteOK(){
         Paciente pacienteSalvo;
-
+        log.info("Salvando paciente ... ");
         pacienteSalvo = pacienteService.salvar(paciente);
-
+        log.info("Paciente salvo: " + pacienteSalvo);
         Assertions.assertNotNull(pacienteSalvo.getId());
 
     }
