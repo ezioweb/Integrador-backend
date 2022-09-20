ebb4c79fb8c3004b03a7062e11f08edb434e2fe1
ezioweb <ezioweb@hotmail.com>
2022-09-19 22:20:51 -0300

    commit Segurança01

---
+++ odonto-grupo1/src/main/java/com/dh/odontogrupo1/CreateUserRun.java
@@ -0,0 +1,12 @@
+package com.dh.odontogrupo1;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class CreateUserRun implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+    }
+}
---
+++ odonto-grupo1/src/main/java/com/dh/odontogrupo1/model/Usuario.java
@@ -0,0 +1,69 @@
+package com.dh.odontogrupo1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 6)
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty
+    @Size(min = 6)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    //private List<Perfil> perfis;
+
+
+
+    //@Override
+    //public Collection<? extends GrantedAuthority> getAuthorities() {
+    //    return this.perfis;
+    //}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
